4746f69c18b0345cab651c6e32586c8dd2446a1d
Bola Salah <103209637+BolaSalah@users.noreply.github.com>
2024-07-10 14:13:25 +0300

    add a page number as a props to copy this component

--- src/components/ui/demo/Slider.jsx
+++ src/components/ui/demo/Slider.jsx
@@ -10,18 +10,20 @@ import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import instance from '../../../axiosConfig/instance';
 import { useNavigate } from 'react-router-dom';
+import { CircularProgress } from '@mui/material';
 
-export function Slider() {
+export function Slider({ pageNumber, title }) {
   const navigate = useNavigate();
   const [moviesList, setMoviesList] = useState([]);
   const modeState = useSelector((state) => state.mode.mode);
+  const loader = useSelector((state) => state.loader.loader);
 
   // get movie
   const getmovie = async () => {
     await instance
-      .get('movie/popular', { params: { page: 3 } })
+      .get('movie/popular', { params: { page: pageNumber } })
       .then((res) => {
-        setMoviesList(res.data.results.slice(7, 19));
+        setMoviesList(res.data.results.slice(1,));
       })
       .catch((err) => {
         console.log(err);
@@ -41,39 +43,46 @@ export function Slider() {
   };
 
   return (
-    <div className='flex justify-center flex-col my-12'>
+    <>
+    {
+      loader?(
+        <div className = 'flex justify-center items-center w-full h-[85vh]' >
+          <CircularProgress />
+        </div>
+      ) : (
+    <div className='flex justify-center flex-col md:my-20 my-5'>
       <div className='flex justify-center'>
         <Carousel
-          opts={{
+          opts={ {
             align: 'start',
-          }}
+          } }
         >
-          <div className='flex  pb-4'>
+          <div className='mb-6 flex items-center'>
             <div
-              className={`w-3 rounded-md h-10 ${
-                modeState == 'light' ? 'bg-[#9a90af]' : 'bg-[#00df9a]'
-              }`}
+              className={ `w-3 rounded-md h-10 ${modeState == 'light' ? 'bg-[#9a90af]' : 'bg-[#00df9a]'
+                }` }
             ></div>
-            <div className='mb-3 ms-3  text-[25px] font-bold'>
-              Fan favorites
-            </div>
+            <div className='ms-3   md:text-[25px] font-bold'>{ title }</div>
           </div>
           <CarouselContent>
-            {moviesList.map((movie, index) => (
-              <CarouselItem key={index} className='lg:basis-[20%] basis-[40%]'>
-                <div>
-                  <Card>
-                    <CardContent className='flex items-center  '>
+            { moviesList.map((movie, index) => (
+              <CarouselItem
+                key={ index }
+                className=' lg:w-[22%] md:w-[27%] sm:w-[30%] w-[40%]  md:h-72 '
+              >
+                <div className='h-full'>
+                  <Card className='h-full'>
+                    <CardContent className='flex items-center h-full '>
                       <img
-                        className='hover:opacity-60 rounded-xl'
-                        src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                        className='hover:opacity-60 rounded-xl h-full w-full'
+                        src={ `https://image.tmdb.org/t/p/w500/${movie.poster_path}` }
                         // src={`https://image.tmdb.org/t/p/w500/gKkl37BQuKTanygYQG1pyYgLVgf.jpg`}
                         alt=''
                       />
                       <div
-                        onClick={() => {
+                        onClick={ () => {
                           toDetails(movie.id);
-                        }}
+                        } }
                         className='w-full h-full absolute flex justify-center items-center flex-col
                                  bg-black text-white transition duration-1000 
                                    opacity-0 hover:opacity-70 hover:cursor-pointer'
@@ -84,12 +93,16 @@ export function Slider() {
                   </Card>
                 </div>
               </CarouselItem>
-            ))}
+            )) }
           </CarouselContent>
           <CarouselPrevious />
           <CarouselNext />
         </Carousel>
       </div>
     </div>
-  );
+  )
+}
+    </>
+
+      );
 }
