b2bfd704d1703b059603d509114e5640483f39a4
Bola Salah <103209637+BolaSalah@users.noreply.github.com>
2024-07-13 17:28:09 +0300

    edit import notFound image

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import notfounfImage from "../../../public/notFound.jpg"
 import { Link } from 'react-router-dom';
 
 export default function NotFound() {
   return (
     <div className='flex justify-center h-[100vh]'>
       <Link to={''}>
-        <img src={notfounfImage} alt='notfounfImage' />
+        <img src={'/notFound.jpg'} alt='notfounfImage' />
       </Link>
     </div>
   );
