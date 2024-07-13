df0f34d3974c3467c6e6f553918dc37baf428e9e
Bola Salah <103209637+BolaSalah@users.noreply.github.com>
2024-07-13 23:28:09 +0300

    add vercel/analytics package

--- src/AppLayout.jsx
+++ src/AppLayout.jsx
@@ -3,6 +3,7 @@ import ResponsiveAppBar from './components/Navbar/ResponsiveAppBar';
 import { Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Footer from './components/Footer/Footer';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function AppLayout() {
   const modeState = useSelector((state) => state.mode.mode);
